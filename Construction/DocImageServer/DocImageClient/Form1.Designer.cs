881b10b00f050ce1f1f941eb70ccb2e77085f348
Jordan Zabar <jordan-zabar77@hotmail.com>
2018-03-22 12:29:22 -0400

    Merge branch 'master' of https://github.com/JoeMcl99/DocImage-Capstone

--- Construction/DocImageServer/DocImageClient/Form1.Designer.cs
+++ Construction/DocImageServer/DocImageClient/Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.txtExample = new System.Windows.Forms.TextBox();
+            this.exit_Btn = new System.Windows.Forms.Button();
+            this.account_Btn = new System.Windows.Forms.Button();
+            this.chkList_Btn = new System.Windows.Forms.Button();
+            this.single_Btn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtExample
@@ -38,11 +42,51 @@
             this.txtExample.Size = new System.Drawing.Size(100, 20);
             this.txtExample.TabIndex = 0;
             // 
+            // exit_Btn
+            // 
+            this.exit_Btn.Location = new System.Drawing.Point(330, 156);
+            this.exit_Btn.Name = "exit_Btn";
+            this.exit_Btn.Size = new System.Drawing.Size(75, 23);
+            this.exit_Btn.TabIndex = 8;
+            this.exit_Btn.Text = "Exit";
+            this.exit_Btn.UseVisualStyleBackColor = true;
+            // 
+            // account_Btn
+            // 
+            this.account_Btn.Location = new System.Drawing.Point(204, 156);
+            this.account_Btn.Name = "account_Btn";
+            this.account_Btn.Size = new System.Drawing.Size(75, 23);
+            this.account_Btn.TabIndex = 7;
+            this.account_Btn.Text = "Account Info";
+            this.account_Btn.UseVisualStyleBackColor = true;
+            // 
+            // chkList_Btn
+            // 
+            this.chkList_Btn.Location = new System.Drawing.Point(80, 156);
+            this.chkList_Btn.Name = "chkList_Btn";
+            this.chkList_Btn.Size = new System.Drawing.Size(75, 23);
+            this.chkList_Btn.TabIndex = 6;
+            this.chkList_Btn.Text = "Check List";
+            this.chkList_Btn.UseVisualStyleBackColor = true;
+            // 
+            // single_Btn
+            // 
+            this.single_Btn.Location = new System.Drawing.Point(-29, 156);
+            this.single_Btn.Name = "single_Btn";
+            this.single_Btn.Size = new System.Drawing.Size(75, 23);
+            this.single_Btn.TabIndex = 5;
+            this.single_Btn.Text = "Single Check";
+            this.single_Btn.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(377, 334);
+            this.ClientSize = new System.Drawing.Size(813, 496);
+            this.Controls.Add(this.exit_Btn);
+            this.Controls.Add(this.account_Btn);
+            this.Controls.Add(this.chkList_Btn);
+            this.Controls.Add(this.single_Btn);
             this.Controls.Add(this.txtExample);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -54,6 +98,10 @@
         #endregion
 
         private System.Windows.Forms.TextBox txtExample;
+        private System.Windows.Forms.Button exit_Btn;
+        private System.Windows.Forms.Button account_Btn;
+        private System.Windows.Forms.Button chkList_Btn;
+        private System.Windows.Forms.Button single_Btn;
     }
 }
 
